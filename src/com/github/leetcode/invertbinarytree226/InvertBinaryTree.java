b10b4a1a360fc1cfff173087f250328ca41b0b1e
EvilNOP <evilnops@gmail.com>
2020-07-07 09:59:58 +0800

    feat(invertbinarytree226): implement solution to invert binary tree recursively and bfs

--- src/com/github/leetcode/invertbinarytree226/InvertBinaryTree.java
+++ src/com/github/leetcode/invertbinarytree226/InvertBinaryTree.java
@@ -1,8 +1,58 @@
 package com.github.leetcode.invertbinarytree226;
 
-final class InvertBinaryTree {
+import java.util.LinkedList;
+import java.util.Queue;
 
-    public static void main(String[] args) {
-        System.out.println("Invert Binary Tree");
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+
+    public TreeNode invertTreeRecursively(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTreeRecursively(root.left);
+
+        root.left = invertTreeRecursively(root.right);
+        root.right = left;
+
+        return root;
+    }
+
+    public TreeNode invertTreeBFS(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+
+            TreeNode tempLeft = current.left;
+            current.left = current.right;
+            current.right = tempLeft;
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+
+        return root;
     }
 }
