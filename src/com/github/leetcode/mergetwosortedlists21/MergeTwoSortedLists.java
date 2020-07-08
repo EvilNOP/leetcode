b39bc9e7eda5c96bd1fd1e1eda1eaed69aacaa5b
EvilNOP <evilnops@gmail.com>
2020-07-08 23:08:50 +0800

    feat(mergetwosortedlists): solve problem `MergeTwoSortedLists`

---
+++ src/com/github/leetcode/mergetwosortedlists21/MergeTwoSortedLists.java
@@ -0,0 +1,42 @@
+package com.github.leetcode.mergetwosortedlists21;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode sentinel = new ListNode(-1);
+        ListNode previous = sentinel;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                previous.next = l1;
+                l1 = l1.next;
+            } else {
+                previous.next = l2;
+                l2 = l2.next;
+            }
+
+            previous = previous.next;
+        }
+
+        previous.next = l1 == null ? l2 : l1;
+
+        return sentinel.next;
+    }
+}
