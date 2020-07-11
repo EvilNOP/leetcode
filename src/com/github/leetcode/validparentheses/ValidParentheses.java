879ce7763a294ea0d498a7c822823bb9bd8f9b9a
EvilNOP <evilnops@gmail.com>
2020-07-11 09:58:39 +0800

    feat(validparentheses): solve problem `ValidParentheses`

---
+++ src/com/github/leetcode/validparentheses/ValidParentheses.java
@@ -0,0 +1,36 @@
+package com.github.leetcode.validparentheses;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    private final HashMap<Character, Character> mappings;
+
+    public ValidParentheses() {
+        this.mappings = new HashMap<>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (mappings.containsKey(c)) {
+                char topElement = stack.isEmpty() ? '#' : stack.pop();
+
+                if (topElement != mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
