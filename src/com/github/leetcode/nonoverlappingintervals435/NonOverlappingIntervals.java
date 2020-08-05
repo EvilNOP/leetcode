c94169a58f556f8cf7c1d197c25734a347c7544d
EvilNOP <evilnops@gmail.com>
2020-08-05 21:58:47 +0800

    feat(435): solve problem `NonOverlappingIntervals`

---
+++ src/com/github/leetcode/nonoverlappingintervals435/NonOverlappingIntervals.java
@@ -0,0 +1,43 @@
+package com.github.leetcode.nonoverlappingintervals435;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class NonOverlappingIntervals {
+
+    private boolean isOverlapping(int[] a, int[] b) {
+        return a[1] > b[0];
+    }
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(intervals, new IntervalComparator());
+        int[] dp = new int[intervals.length];
+        dp[0] = 1;
+
+        for (int i = 1; i < intervals.length; i++) {
+            int max = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (!isOverlapping(intervals[j], intervals[i])) {
+                    max = Math.max(dp[j], max);
+                }
+            }
+
+            dp[i] = (max + 1);
+        }
+
+        return intervals.length - dp[intervals.length - 1];
+    }
+
+    class IntervalComparator implements Comparator<int[]> {
+
+        @Override
+        public int compare(int[] a, int[] b) {
+            return a[0] - b[0];
+        }
+    }
+}
