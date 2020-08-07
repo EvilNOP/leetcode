be25e3e7543fa59823ce6dba415387dc079924b4
EvilNOP <evilnops@gmail.com>
2020-08-07 19:17:35 +0800

    feat(231): solve problem `PowerOfTwo`

---
+++ src/com/github/leetcode/poweroftwo231/PowerOfTwo.java
@@ -0,0 +1,12 @@
+package com.github.leetcode.poweroftwo231;
+
+public class PowerOfTwo {
+
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+        }
+
+        return ((long) n & ((long) n - 1)) == 0;
+    }
+}
