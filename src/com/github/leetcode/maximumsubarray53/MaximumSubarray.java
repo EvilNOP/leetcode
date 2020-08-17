213575f631f756c4bc52330728fb4a1c16a332d9
EvilNOP <evilnops@gmail.com>
2020-08-17 14:02:01 +0800

    feat(53): solve problem `MaximumSubarray` recursively

--- src/com/github/leetcode/maximumsubarray53/MaximumSubarray.java
+++ src/com/github/leetcode/maximumsubarray53/MaximumSubarray.java
@@ -32,4 +32,43 @@ public class MaximumSubarray {
 
         return maxSum;
     }
+
+    public int maxSubArrayRecursively(int[] nums, int low, int high) {
+        if (low == high) {
+            return nums[low];
+        }
+
+        int mid = (low + high) / 2;
+        int leftSum = maxSubArrayRecursively(nums, low, mid);
+        int rightSum = maxSubArrayRecursively(nums, mid + 1, high);
+        int crossSum = findMaximumCrossingSubarray(nums, low, mid, high);
+
+        return Math.max(Math.max(leftSum, rightSum), crossSum);
+    }
+
+    public int findMaximumCrossingSubarray(int[] nums, int low, int mid, int high) {
+        int leftMax = Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = mid; i >= low; i--) {
+            sum += nums[i];
+
+            if (sum > leftMax) {
+                leftMax = sum;
+            }
+        }
+
+        sum = 0;
+
+        for (int i = mid + 1; i <= high; i++) {
+            sum += nums[i];
+
+            if (sum > rightMax) {
+                rightMax = sum;
+            }
+        }
+
+        return leftMax + rightMax;
+    }
 }
