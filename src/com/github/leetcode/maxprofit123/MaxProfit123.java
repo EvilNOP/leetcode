592fa1c0f3fad4a3c0e061fb1a7f1f441e840b74
EvilNOP <evilnops@gmail.com>
2020-08-28 10:35:31 +0800

    feat(123): solve problem `MaxProfit` using dp

--- src/com/github/leetcode/maxprofit123/MaxProfit123.java
+++ src/com/github/leetcode/maxprofit123/MaxProfit123.java
@@ -3,24 +3,28 @@ package com.github.leetcode.maxprofit123;
 public class MaxProfit123 {
 
     public int maxProfit(int[] prices) {
-        int maxDeal = 2;
-        int[][][] dp = new int[prices.length][maxDeal + 1][2];
+//        int maxDeal = 2;
+//        int[][][] dp = new int[prices.length][maxDeal + 1][2];
 
-        for (int i = 0; i < prices.length; i++) {
-            for (int k = maxDeal; k >= 1; k--) {
-                if (i - 1 == -1) {
-                    dp[i][k][0] = 0;
-                    dp[i][k][1] = -prices[i];
+        int dpI20 = 0;
+        int dpI21 = -prices[0];
+        int dpI10 = 0;
+        int dpI11 = -prices[0];
 
-                    continue;
-                }
+        for (int i = 1; i < prices.length; i++) {
+//            dp[i][2][0] = Math.max(dp[i - 1][2][1] + prices[i], dp[i - 1][2][0]);
+//            dp[i][2][1] = Math.max(dp[i - 1][1][0] - prices[i], dp[i - 1][2][1]);
+//            dp[i][1][0] = Math.max(dp[i - 1][1][1] + prices[i], dp[i - 1][1][0]);
+//            dp[i][1][1] = Math.max(dp[i - 1][0][0] - prices[i], dp[i - 1][1][1]);
 
-                dp[i][k][0] = Math.max(dp[i - 1][k][1] + prices[i], dp[i - 1][k][0]);
-                dp[i][k][1] = Math.max(dp[i - 1][k - 1][0] - prices[i], dp[i - 1][k][1]);
-            }
+            dpI20 = Math.max(dpI21 + prices[i], dpI20);
+            dpI21 = Math.max(dpI10 - prices[i], dpI21);
+            dpI10 = Math.max(dpI11 + prices[i], dpI10);
+            dpI11 = Math.max(-prices[i], dpI11);
         }
 
-        return dp[prices.length - 1][maxDeal][0];
+//        return dp[prices.length - 1][maxDeal][0];
+        return dpI20;
     }
 
     public int maxProfit(int[] prices, int buy, int deal) {
