427ad82ef2a111bbc35e1f15134e1c11c8cb396c
EvilNOP <evilnops@gmail.com>
2020-08-30 14:59:30 +0800

    feat(167): solve problem `TwoSum`

--- src/com/github/leetcode/twosum1/TwoSum1.java
+++ src/com/github/leetcode/twosum1/TwoSum1.java
@@ -2,7 +2,7 @@ package com.github.leetcode.twosum1;
 
 import java.util.HashMap;
 
-public class TwoSum {
+public class TwoSum1 {
 
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
