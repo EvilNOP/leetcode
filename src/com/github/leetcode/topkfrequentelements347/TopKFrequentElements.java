f381bf5710e45c88d2a8c5d7005ce0396f906b88
EvilNOP <evilnops@gmail.com>
2020-09-08 21:45:58 +0800

    feat(347): solve problem `TopKFrequentElements` using partition of quick sort

--- src/com/github/leetcode/topkfrequentelements347/TopKFrequentElements.java
+++ src/com/github/leetcode/topkfrequentelements347/TopKFrequentElements.java
@@ -1,36 +1,87 @@
 package com.github.leetcode.topkfrequentelements347;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class TopKFrequentElements {
 
-    public static int[] topKFrequent(int[] nums, int k) {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(m -> m[1]));
+
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                assert queue.peek() != null;
+
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+
+        int[] ret = new int[k];
+
+        for (int i = 0; i < k; ++i) {
+            ret[i] = Objects.requireNonNull(queue.poll())[0];
+        }
+
+        return ret;
+    }
+
+    public int[] topKFrequentQuickSort(int[] nums, int k) {
         HashMap<Integer, Integer> occurrences = new HashMap<>();
 
         for (int num : nums) {
             occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> priorityQueue = new PriorityQueue<>(
-                (o1, o2) -> o2.getValue().compareTo(o1.getValue())
-        );
+        List<int[]> values = new ArrayList<>();
 
-        for (Map.Entry<Integer, Integer> integerIntegerEntry : occurrences.entrySet()) {
-            priorityQueue.offer(integerIntegerEntry);
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+            values.add(new int[]{key, value});
         }
 
-        int[] topK = new int[k];
+        int[] ans = new int[k];
+        quickSort(0, values.size() - 1, 0, k, values, ans);
 
-        for (int i = 0; i < k; i++) {
-            topK[i] = priorityQueue.poll().getKey();
-        }
-
-        return topK;
+        return ans;
     }
 
-    public static void main(String[] args) {
-        System.out.println(topKFrequent(new int[]{1}, 1));
+    public void quickSort(int start, int end, int cursor, int k, List<int[]> values, int[] ans) {
+        int picked = (int) (Math.random() * (end - start + 1)) + start;
+        Collections.swap(values, start, picked);
+
+        int pivot = values.get(start)[1];
+        int index = start;
+
+        for (int i = start + 1; i <= end; i++) {
+            if (values.get(i)[1] >= pivot) {
+                Collections.swap(values, ++index, i);
+            }
+        }
+
+        Collections.swap(values, start, index);
+
+        if (k <= (index - start)) {
+            quickSort(start, index - 1, cursor, k, values, ans);
+        } else {
+            for (int i = start; i <= index; i++) {
+                ans[cursor++] = values.get(i)[0];
+            }
+
+            if (k > (index - start + 1)) {
+                quickSort(index + 1, end, cursor, k - (index - start + 1), values, ans);
+            }
+        }
     }
 }
