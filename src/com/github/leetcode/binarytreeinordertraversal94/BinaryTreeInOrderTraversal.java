350ec00de74bb0468ae4303ad82b590ebc383f65
EvilNOP <evilnops@gmail.com>
2020-09-14 00:23:35 +0800

    feat(94): solve problem `BinaryTreeInOrderTraversal` using stack

--- src/com/github/leetcode/binarytreeinordertraversal94/BinaryTreeInOrderTraversal.java
+++ src/com/github/leetcode/binarytreeinordertraversal94/BinaryTreeInOrderTraversal.java
@@ -1,6 +1,8 @@
 package com.github.leetcode.binarytreeinordertraversal94;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 class TreeNode {
@@ -25,6 +27,24 @@ public class BinaryTreeInOrderTraversal {
         inorderTraversal(root.left);
         traversal.add(root.val);
         inorderTraversal(root.right);
+
+        return traversal;
+    }
+
+    public List<Integer> inorderTraversalWithStack(TreeNode root) {
+        List<Integer> traversal = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            traversal.add(root.val);
+            root = root.right;
+        }
 
         return traversal;
     }
