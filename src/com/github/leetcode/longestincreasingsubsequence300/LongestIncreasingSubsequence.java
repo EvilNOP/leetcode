fd83a8020b292d9b4a28e198b8b18ca5573c07c3
EvilNOP <evilnops@gmail.com>
2020-09-16 10:27:47 +0800

    feat(300): solve problem `LongestIncreasingSubsequence` using greedy algorithm

--- src/com/github/leetcode/longestincreasingsubsequence300/LongestIncreasingSubsequence.java
+++ src/com/github/leetcode/longestincreasingsubsequence300/LongestIncreasingSubsequence.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LongestIncreasingSubsequence {
 
-    public static int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
         if (n == 0) {
@@ -31,5 +31,41 @@ public class LongestIncreasingSubsequence {
         }
 
         return maxLen;
+    }
+
+    public int lengthOfLISGreedy(int[] nums) {
+        int n = nums.length;
+
+        if (n == 0) {
+            return 0;
+        }
+
+        int end = 0;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > dp[end]) {
+                end++;
+                dp[end] = nums[i];
+            } else {
+                int left = 0;
+                int right = end;
+
+                while (left < right) {
+                    int mid = (left + right) >>> 1;
+
+                    if (nums[i] > dp[mid]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+
+                dp[left] = nums[i];
+            }
+        }
+
+        return end + 1;
     }
 }
