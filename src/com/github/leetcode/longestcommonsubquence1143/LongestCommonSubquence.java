ecf2570abeed1bb4205ac28d09299aeb1805b0af
EvilNOP <evilnops@gmail.com>
2020-09-18 17:51:15 +0800

    feat(1143): solve problem `LongestCommonSubsequence`

---
+++ src/com/github/leetcode/longestcommonsubquence1143/LongestCommonSubquence.java
@@ -0,0 +1,29 @@
+package com.github.leetcode.longestcommonsubquence1143;
+
+public class LongestCommonSubquence {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+
+        char[] t1 = text1.toCharArray();
+        char[] t2 = text2.toCharArray();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (t2[j - 1] == t1[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
