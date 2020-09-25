df44ca70ca989bea97df39e2981e6db88a5476b2
EvilNOP <evilnops@gmail.com>
2020-09-25 22:58:24 +0800

    feat(236): solve problem `LowestCommonAncestor`

---
+++ src/com/github/leetcode/lowestcommonancestor236/LowestCommonAncestor.java
@@ -0,0 +1,37 @@
+package com.github.leetcode.lowestcommonancestor236;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class LowestCommonAncestor {
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        if (left == null && right == null) {
+            return null;
+        }
+
+        return left == null ? right : left;
+    }
+}
