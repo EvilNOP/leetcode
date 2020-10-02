ee8633f7bed05b2b67cad53bf534a8c559883021
EvilNOP <evilnops@gmail.com>
2020-10-02 09:41:22 +0800

    feat(114): solve problem `BinaryTreePreorderTraversal`

---
+++ src/com/github/leetcode/binarytreepreordertraversal114/BinaryTreePreorderTraversal.java
@@ -0,0 +1,42 @@
+package com.github.leetcode.binarytreepreordertraversal114;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTreePreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preorderTraversal(root, ans);
+        return ans;
+    }
+
+    public void preorderTraversal(TreeNode root, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+
+        ans.add(root.val);
+        preorderTraversal(root.left, ans);
+        preorderTraversal(root.right, ans);
+    }
+}
