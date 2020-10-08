81134ad7a804b347544822d6f16d4b16939f6843
EvilNOP <evilnops@gmail.com>
2020-10-08 16:35:20 +0800

    feat(283): solve problem `MoveZeros`

---
+++ src/com/github/leetcode/movezeros283/MoveZeros.java
@@ -0,0 +1,21 @@
+package com.github.leetcode.movezeros283;
+
+public class MoveZeros {
+
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                ++left;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
