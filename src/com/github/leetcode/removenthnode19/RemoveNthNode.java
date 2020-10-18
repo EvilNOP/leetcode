99be0bb80c964fdd248884b05ff29f093cf6e1d9
EvilNOP <evilnops@gmail.com>
2020-10-19 00:10:15 +0800

    feat(19): polish code

--- src/com/github/leetcode/removenthnode19/RemoveNthNode.java
+++ src/com/github/leetcode/removenthnode19/RemoveNthNode.java
@@ -20,38 +20,20 @@ class ListNode {
 public class RemoveNthNode {
 
     public static ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) {
-            return null;
-        }
-
-        int k = n;
-        ListNode slow = head;
+        ListNode sentinel = new ListNode(0, head);
+        ListNode slow = sentinel;
         ListNode fast = head;
 
-        while (--k > 0 && fast != null) {
+        while (--n >= 0) {
             fast = fast.next;
         }
 
-        if (fast == null) {
-            return head;
-        }
-
-        ListNode sentinel = new ListNode(0);
-        sentinel.next = head;
-
-        ListNode previous = sentinel;
-
-        while (fast.next != null) {
+        while (fast != null) {
             slow = slow.next;
             fast = fast.next;
-            previous = previous.next;
         }
 
-        if (n == 1) {
-            previous.next = null;
-        } else {
-            previous.next = slow.next;
-        }
+        slow.next = slow.next.next;
 
         return sentinel.next;
     }
