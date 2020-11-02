d90921eda35c5449170b724796a67549cb901d72
EvilNOP <evilnops@gmail.com>
2020-11-02 11:44:05 +0800

    feat(139): solve problem `WordBreak`

--- src/com/github/leetcode/wordbreak139/WordBreak139.java
+++ src/com/github/leetcode/wordbreak139/WordBreak139.java
@@ -9,11 +9,17 @@ public class WordBreak139 {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> words = new HashSet<>(wordDict);
 
+        int maxLen = 0;
+
+        for (String word : words) {
+            maxLen = Math.max(maxLen, word.length());
+        }
+
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
 
         for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = i - 1; j >= 0 && j >= i - maxLen; j--) {
                 if (dp[j] && words.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
